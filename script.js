9b3a8de76ab76dab2bfb5290cf2403188bc01e29
Thomas Schaffner <t.f.schaffner@gmail.com>
2018-07-27 23:20:55 -0400

    Clean code

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-// $(document).ready(function() {
-// 	initGraphic();
-// });
-
-
-// function initGraphic() {
-
-// }
-
-
 var X_INNER_SPACING = 2; // px
 var X_OUTER_MARGIN = 5; // px
 
@@ -82,8 +72,6 @@ function generateGraphic(actmap, teachtots) {
 		return second[1] - first[1];
 	});
 
-	// console.log(ttitems);
-
 	var totalcount = 0;
 	for (var i = 0; i < ttitems.length; i++) {
 		totalcount += ttitems[i][1];
@@ -94,14 +82,6 @@ function generateGraphic(actmap, teachtots) {
 	var ttwidths = {}
 	for (var i = 0; i < ttitems.length; i++) {
 		ttwidths[ttitems[i][0]] = Math.floor((ttitems[i][1] / totalcount) * xwidth);
-		// console.log("!!!!!!!!!!!!!!!");
-		// console.log(i);
-		// console.log(ttitems[i]);
-		// console.log(ttitems[i][1]);
-		// console.log(ttitems[i][1] / totalcount);
-		// console.log((ttitems[i][1] / totalcount) * xwidth);
-		// console.log(Math.floor((ttitems[i][1] / totalcount) * xwidth));
-		// console.log("iiiiiiiiiiiiiii");
 	}
 
 	var xoffsets = {}
@@ -118,53 +98,10 @@ function generateGraphic(actmap, teachtots) {
 		ttran.push(xoffsets[ttitems[i][1]]);
 	}
 
-	// var x = d3.scaleOrdinal()
-	// 			.domain(ttdom)
-	// 			.range(ttran);
-
-	// console.log("widthts");
-	// console.log(ttwidths);
-	// console.log("offsets");
-	// console.log(xoffsets);
-
-	// TODO(tfs): Adjust x widths based on teacher totals
-	// var x = d3.scaleBand()
-	// 	.domain(data.map(function(d) { return d.teacherActivity; }))
-	// 	.rangeRound([0, width])
-	// 	.padding(0.1)
-	// 	.align(0.1);
-
 	var y = d3.scaleLinear()
 		.rangeRound([height, 0]);
 
-	// var z = d3.scaleOrdinal()
-	// 	.range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
-
 	var z = d3.scaleOrdinal(d3.schemeCategory10);
-
-	// var stack = d3.stack()
-	// 	.offset(d3.stackOffsetExpand);
-
-	// var circles = self.g.selectAll("circle")
- //            .data(nodes, self.constancy);
-
- 	// var bars = g.selectAll("rect")
- 	// 			.data()
-
- 	// var serie = g.selectAll(".serie")
- 	// 			.data(stack.keys(data.columns.slice(1))(data))
- 	// 			.enter().append("g")
- 	// 				.attr("class", "serie")
- 	// 				.attr("fill", function(d) { return z(d.key) });
-
-
-
- 	// serie.selectAll("rect")
- 	// 	.data(function(d) { return d; })
- 	// 	.enter().append("rect")
- 	// 		.attr("x", function(d) { return x(d.data.teacherActivity) })
- 	// 		.attr("y", function(d) { return y(d[])})
-
 
  	// REF: https://bl.ocks.org/mbostock/b5935342c6d21928111928401e2c8608
  	var series = d3.stack()
@@ -185,8 +122,6 @@ function generateGraphic(actmap, teachtots) {
 	subgs.selectAll("rect")
 		.data(function(d) { return d; })
 		.enter().append("rect")
-			// .attr("width", x.bandwidth()) // TODO(tfs): Scale based on teachtots
-			// .attr("x", function(d) { return x(d.data.teacherActivity); })
 			.attr("width", function(d) {
 				return ttwidths[d.data.teacherActivity];
 			})
@@ -219,29 +154,6 @@ function generateGraphic(actmap, teachtots) {
 					.attr("x", 2)
 					.attr("y", -2)
 			});
-	
-	// subgs.selectAll("text")
-	// 	.data(function(d) { return d; })
-	// 	.enter().append("text")
-	// 		.attr("text-anchor", "middle")
-	// 		.attr("x", function(d) { return xoffsets[d.data.teacherActivity] + ttwidths[d.data.teacherActivity]/2 })
-	// 		.attr("y", function(d) { return y(d[1]) + ((y(d[0]) - y(d[1])) / 2) - 6 })
-	// 		.attr("dy", "0.75em")
-	// 		.attr("fill", "#000")
-	// 		.attr("id", function(d) { return "text_" + d.data.teacherActivity.replace(/\ /g, "") + "_" + d.key.replace(/\ /g, "") })
-	// 		.attr("class", function(d) {
-	// 			if ((y(d[0]) - y(d[1])) <= 0) {
-	// 				return "bar-title empty hidden-title";
-	// 			} else {
-	// 				return "bar-title non-empty hidden-title";
-	// 			}
-	// 		})
-	// 		.text(function(d) { return d.key; });
-
-			// .append("text")
-			// 	.attr("text-anchor", "middle")
-			// 	.text(function(d) { console.log(d); })
-			// 	.attr("transform", "translate(" + (xoffsets[d.data.teacherActivity] + (ttwidths[d.data.teacherActivity]/2)) + "," + (y(d[1] + ((y(d[0]) - y(d[1])) / 2))) + ")");
 
 	var botg = svg.append("g")
 		.attr("fill", "none")
@@ -249,7 +161,6 @@ function generateGraphic(actmap, teachtots) {
 		.attr("font-family", "sans-serif")
 		.attr("text-anchor", "middle")
 		.attr("transform", "translate(" + margin.left + "," + (y(0) + margin.top) + ")");
-		// .call(d3.axisBottom().scale(x));
 	
 	botg.append("path")
 			.attr("class", "domain")
@@ -262,13 +173,6 @@ function generateGraphic(actmap, teachtots) {
 			.attr("class", "tick")
 			.attr("opacity", "1")
 			.attr("transform", "translate(" + (X_OUTER_MARGIN + xoffsets[ttdom[i]] + (ttwidths[ttdom[i]]/2)) + ",0)");
-
-		// console.log("!!!!!!!!!!!!!!!!!");
-		// console.log(X_OUTER_MARGIN);
-		// console.log(ttdom[i]);
-		// console.log(xoffsets[ttdom[i]]);
-		// console.log(ttwidths[ttdom[i]]/2);
-		// console.log("iiiiiiiiiiiiiiiii");
 
 		newg.append("line")
 			.attr("stroke", "#000")
@@ -299,7 +203,6 @@ function generateGraphic(actmap, teachtots) {
 		.style("opacity", 0.5);
 
 	tooltip.append("text")
-		// .attr("x", 15)
 		.attr("dy", "1.2em")
 		.style("text-anchor", "front")
 		.attr("font-size", "12px")
@@ -327,7 +230,6 @@ var exp = {};
 
 function handleMouseEnter(d) {
 	var selector = "#text_" + d.data.teacherActivity.replace(/\ /g, "") + "_" + d.key.replace(/\ /g, "");
-	// d3.select(selector).classed("hidden-title", false);
 	console.log(selector);
 	$(selector).removeClass("hidden-title");
 }
@@ -335,7 +237,6 @@ function handleMouseEnter(d) {
 
 function handleMouseLeave(d) {
 	var selector = "#text_" + d.data.teacherActivity.replace(/\ /g, "") + "_" + d.key.replace(/\ /g, "");
-	// d3.select(selector).classed("hidden-title", true);
 	console.log(selector);
 	$(selector).addClass("hidden-title");
 }
